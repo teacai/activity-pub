494edf021aed8f15bb7a6b9cb4676888701a1e3e
Igor Teaca <igor@teacai.com>
2023-07-14 07:57:03 +0100

    Maven project setup

---
+++ src/main/java/com/teacai/activitypub/ActivityPub.java
@@ -0,0 +1,5 @@
+package com.teacai.activitypub;
+
+public class ActivityPub {
+
+}
---
+++ src/test/java/com/teacai/activitypub/ActivityPubTests.java
@@ -0,0 +1,11 @@
+package com.teacai.activitypub;
+
+import org.junit.jupiter.api.Test;
+
+class ActivityPubTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
